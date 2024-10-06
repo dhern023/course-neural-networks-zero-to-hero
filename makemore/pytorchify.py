6a522b020720c20722ecd366abf1ea26c5c61c51
dhern023 <dhern023@ucr.edu>
2024-10-06 19:32:34 -0400

    Finish off the pytorchify code

--- makemore/pytorchify.py
+++ makemore/pytorchify.py
@@ -3,8 +3,10 @@ Different from the actual implementation in that we use pytorch completely.
 I'm interested in the activation statistics
 NOTE: torch.nn.Linear doesn't allow us to easily adjust fan-in, so our 
 saturation won't ever be "bad"
+TODO: Use the safetensor for reading the dataset
 """
 import matplotlib.pyplot as plt
+import matplotlib.lines
 import pathlib
 import random
 import torch
@@ -114,6 +116,8 @@ Ytr, Ydev, Yts = ys.tensor_split((n1, n2), dim=0)
 class MLP(torch.nn.Module):
 
     def __init__(self, num_embeddings, size_dimension, size_input, size_hidden, size_output):
+        """
+        """
         super(MLP, self).__init__() # override class
         self.C = torch.nn.Embedding(num_embeddings,embedding_dim=size_dimension)
         self.W = torch.nn.Linear(size_input*size_dimension, size_hidden)
@@ -123,29 +127,90 @@ class MLP(torch.nn.Module):
         self.logits = torch.nn.Linear(size_hidden, size_output)
         self.activation = torch.nn.Tanh()
 
+        # Initialize weights
+        self.init_weights() # avoid the vanishing gradient problem
+
+    def init_weights(self):
+        """
+        Pytorch torch.nn.Linear layers are automatically initialized using a variant of Kaiming (He) initialization.
+        Problem is this works poorly with tanh, whose activations benefit more from Xavier (Godot).
+        As is, we would run into the vanishing gradient problem if we didn't invoke batch_normalization
+        """
+        torch.nn.init.kaiming_uniform_(self.W.weight, nonlinearity='tanh')
+        torch.nn.init.zeros_(self.W.bias)
+        for hidden in self.hiddens:
+            torch.nn.init.kaiming_uniform_(hidden.weight, nonlinearity='tanh')
+            torch.nn.init.zeros_(hidden.bias)
+        torch.nn.init.kaiming_uniform_(self.logits.weight, nonlinearity='tanh')
+        torch.nn.init.zeros_(self.logits.bias)
+
     def forward(self, vector_input):
         """
         Requires vector of (num_ngrams[indices], size_input)
         """
-        activations = []
-
         embed = self.C(vector_input) # (num_ngrams[indices], size_input, size_dimension)
         embed = embed.view(embed.shape[0], -1) # reshape
         layer = self.W(embed) # (size_dimension, size_hidden)
-        layer = self.activation(layer)
-        activations.append(layer)
+        layer = self.activation(layer)        
 
         for hidden in self.hiddens:
             layer = hidden(layer) # (size_hidden, size_hidden)
             layer = self.activation(layer)
-            activations.append(layer)
 
         layer = self.logits(layer) # (size_hidden, size_output)
 
-        return layer, activations
+        return layer
+
+def plot_grad_flow(instance_model):
+    """
+    Plots the gradients flowing through different layers in the net during training.
+    Can be used for checking for possible gradient vanishing / exploding problems.
+    """
+    ave_grads = []
+    max_grads = []
+    layers = []
+
+    for name, parameter in instance_model.named_parameters():
+        if parameter.requires_grad and "bias" not in name:
+            layers.append(name)
+            ave_grads.append(parameter.grad.abs().mean().item())
+            max_grads.append(parameter.grad.abs().max().item())
+
+    # Convert lists to tensors
+    ave_grads_tensor = torch.tensor(ave_grads)
+    max_grads_tensor = torch.tensor(max_grads)
+
+    # Create histogram bins
+    bins = torch.linspace(0, len(max_grads), len(max_grads) + 1)
+
+    # Compute histograms
+    hist_ave_grads = torch.histogram(ave_grads_tensor, bins=bins)
+    hist_max_grads = torch.histogram(max_grads_tensor, bins=bins)
+
+    # Plotting
+    plt.bar(hist_max_grads.bin_edges[:-1], hist_max_grads.hist, alpha=0.1, lw=1, color="c")
+    plt.bar(hist_ave_grads.bin_edges[:-1], hist_ave_grads.hist, alpha=0.1, lw=1, color="b")
+    plt.hlines(0, 0, len(ave_grads), lw=2, color="k")
+    plt.xticks(torch.arange(len(ave_grads)), layers, rotation="vertical")
+    plt.xlim(left=0, right=len(ave_grads))
+    plt.ylim(bottom=-0.001, top=0.02)  # zoom in on the lower gradient regions
+    plt.xlabel("Layers")
+    plt.ylabel("Average Gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.legend([matplotlib.lines.Line2D([0], [0], color="c", lw=4),
+                matplotlib.lines.Line2D([0], [0], color="b", lw=4),
+                matplotlib.lines.Line2D([0], [0], color="k", lw=4)], ['max-gradient', 'mean-gradient', 'zero-gradient'])
+    plt.savefig(DIR_OUT / "activation-flow.png")
+
 
 def train_model(instance_model, num_epochs, dataloader):
+    """
+    https://web.stanford.edu/~nanbhas/blog/forward-hooks-pytorch/
 
+    Use a hook to save the activation's values and gradients each time it's called
+        for the linear layer and each hidden layer
+    """
     LEARNING_RATE = 0.1 # discovered empirocally
     optimizer = torch.optim.SGD(instance_model.parameters(), lr=LEARNING_RATE)
     loss_function = torch.nn.CrossEntropyLoss()
@@ -154,27 +219,57 @@ def train_model(instance_model, num_epochs, dataloader):
     # tensor_losses = torch.zeros(size=(num_epochs, len(Xtr)))
     epoch_decay = num_epochs*0.9
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[epoch_decay], gamma=0.1)
-    list_activations_all = []
+
+    # a dict to store the activations
+    dict_activations = {}
+    def get_activation(name):
+        # the hook signature
+        def hook(model, input, output):
+            dict_activations.setdefault(name, [])
+            dict_activations[name].append(output.detach())
+        return hook
+
+    dict_gradients = {}
+    def get_gradient(name):
+        # the hook signature
+        def hook(module, grad_input, grad_output):
+            dict_gradients.setdefault(name, [])
+            dict_gradients[name].append(grad_output[0])
+        return hook
+
+    # register forward hooks on the layers of choice
+    h1 = model.activation.register_forward_hook(get_activation('activation'))
+    b1 = model.activation.register_full_backward_hook(get_gradient('activation'))
+    dict_out = {
+        "activations": [],
+        "gradients": []
+    }    
+
     for i in tqdm.tqdm(range(num_epochs), total = num_epochs//1000):
         for batch in dataloader:
             model.zero_grad() # zero out the gradients
             xtr, ytr = batch
-            log_probs, activations = model(xtr)
+            log_probs = model(xtr)
             loss = loss_function(log_probs, ytr)
 
             loss.backward() # compute gradient
             optimizer.step() # update parameters
 
             list_loss.append(loss.log10().item())
-            list_activations_all.append(activations)
-            break
+            dict_out["activations"].append(dict_activations['activation'])
+            dict_out["gradients"].append(dict_gradients['activation'])
+
+            # detach the hooks to only save after the first run
+            h1.remove()
+            b1.remove()
 
         if i == epoch_decay:
             print("\tLoss before learning rate decay:", loss.item())
         scheduler.step()
 
     print("\tLoss after learning rate decay:", loss.item())
-    return instance_model, list_loss, list_activations_all
+
+    return instance_model, list_loss, dict_out
 
 SIZE_CONTEXT = 4
 SIZE_DIMENSION=10
@@ -190,33 +285,37 @@ model = MLP(
 dataset = torch.utils.data.TensorDataset(Xtr, Ytr)
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
 NUM_EPOCHS = int(1) # int(2e4) # int(2e5)
-model, list_loss, list_activations_all = train_model(model, NUM_EPOCHS, dataloader)
+model, list_loss, dict_snapshots = train_model(model, NUM_EPOCHS, dataloader)
 plt.plot(list_loss)
-plt.savefig(DIR_OUT / "train-loss-decay-pytorch.png")
+plt.savefig(DIR_OUT / "train-loss-decay-pytorch")
 plt.close()
+
+plot_grad_flow(model)
+
+# Forward-pass activation statistics ==============================================================
 
 plt.figure()
 plt.title(f'Activation Histogram for Each Layers')
 plt.xlabel('Activation Value')
 plt.ylabel('Frequency')
-for i, activation in enumerate(list_activations_all[0]):
+for i, activation in enumerate(dict_snapshots["activations"][0]):
     t = activation
     saturated_percent = (t.abs() > 0.90).float().mean().mul_(100).item()
-    print(f"activation layer {i}: mean {t.mean():0.2f}, std {t.std():0.2f}, saturated: {t}%")
+    print(f"activation layer {i}: mean {t.mean():0.2f}, std {t.std():0.2f}, saturated: {saturated_percent}%")
     tensors_hist = torch.histogram(t.detach().cpu(), bins=50, density=True) 
     plt.plot(tensors_hist.bin_edges[:-1], tensors_hist.hist, label=f'For Layer {i+1}')
 plt.legend()
-plt.savefig(DIR_OUT / "activation-distibution")
+plt.savefig(DIR_OUT / "activation-distibution-properly-initialized")
 
 plt.figure()
 plt.title(f'Activation Gradient Histogram for Each Layers')
 plt.xlabel('Activation Gradient Value')
 plt.ylabel('Frequency')
-for i, activation in enumerate(list_activations_all[0]):
-    t = activation.grad
+for i, activation in enumerate(dict_snapshots["gradients"][0]):
+    t = activation
     saturated_percent = (t.abs() > 0.90).float().mean().mul_(100).item()
-    print(f"activation layer {i}: mean {t.mean():0.2f}, std {t.std():0.2f}, saturated: {t}%")
+    print(f"gradient layer {i}: mean {t.mean():0.2f}, std {t.std():0.2f}, saturated: {saturated_percent}%")
     tensors_hist = torch.histogram(t.detach().cpu(), bins=50, density=True) 
     plt.plot(tensors_hist.bin_edges[:-1], tensors_hist.hist, label=f'For Layer {i+1}')
 plt.legend()
-plt.savefig(DIR_OUT / "activation-gradient-distibution")
+plt.savefig(DIR_OUT / "activation-gradient-distibution-properly-initialized")
