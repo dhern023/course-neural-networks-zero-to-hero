2931cafeafec377e6150602927dd81aca2aa5972
dhern023 <dhern023@ucr.edu>
2025-02-16 02:09:45 -0500

    End to end for attention (single-head)

---
+++ nanoGPT/attention.py
@@ -0,0 +1,96 @@
+"""
+Trains a bigram model on the input text
+"""
+import pathlib
+import torch
+import torch.utils.data
+import sys
+
+# in house
+import _tokenizer
+import models
+
+FNAME_DATA = pathlib.Path(__file__).parent / "input.txt"
+
+g = torch.Generator().manual_seed(2147483647) # default seed from the torch docs, shares memory
+
+if FNAME_DATA.exists():
+    with open(FNAME_DATA, "r") as file:
+        data = file.read()
+else:
+    sys.exit(9) # fail fast
+
+dict_to_idx, dict_to_token = _tokenizer.construct_character_mappings(data)
+list_tokens = _tokenizer.character_encode(data, dict_to_idx)
+vector_tokens = torch.tensor(list_tokens, dtype = torch.long)
+
+SIZE_CONTEXT=8
+SIZE_BATCH=4
+SIZE_VOCAB=len(dict_to_idx)
+SIZE_EMBEDDING_DIM=32
+
+# pad to account for missing data
+remainder = len(vector_tokens) % (SIZE_CONTEXT+1)
+size_pad = (SIZE_CONTEXT+1) - remainder
+vector_tokens = torch.cat((vector_tokens, torch.zeros(size_pad, dtype=vector_tokens.dtype)))
+matrix_tokens = vector_tokens.view(-1, SIZE_CONTEXT+1)
+
+# split train/test
+
+dataset = torch.utils.data.TensorDataset(
+    matrix_tokens[:,:SIZE_CONTEXT], # first SIZE_CONTEXT
+    matrix_tokens[:,1:SIZE_CONTEXT+1] # shift by one to get targets
+)
+dataset_train, dataset_test = torch.utils.data.random_split(dataset, [0.9, 0.1], generator=g) # 90/10
+
+dataloader_train = torch.utils.data.DataLoader(dataset_train, batch_size=SIZE_BATCH, shuffle=True)
+dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=SIZE_BATCH, shuffle=False)
+
+# Train model -------------------------------------------------------------------------------------
+model = models.BigramLanguageModelAttention(SIZE_CONTEXT, SIZE_VOCAB, SIZE_EMBEDDING_DIM, 16)
+xtr, ytr = next(iter(dataloader_train))
+
+logits, loss = model(xtr, ytr)
+
+SIZE_EVALUATE=1000
+
+@torch.no_grad()
+def evaluate_model(instance_model):
+    dict_out = {}
+    instance_model.eval()
+    for (key, dataloader) in zip(("train", "test"), (dataloader_train, dataloader_test)):
+        tensor_losses = torch.zeros(SIZE_EVALUATE)
+        for i, batch in enumerate(dataloader):
+            xs, ys = batch
+            logits, loss = instance_model(xs, ys)
+            tensor_losses[i] = loss.item()
+            if i == SIZE_EVALUATE-1:
+                break
+        dict_out[key] = tensor_losses.mean()
+    instance_model.train()
+    
+    return dict_out
+
+# train model
+optimizer = torch.optim.AdamW(params = model.parameters(), lr = 1e-3)
+NUM_EPOCHS=1 # runs through the dataset
+for epoch in range(NUM_EPOCHS):
+    for i, batch in enumerate(dataloader_train):
+
+        if i % SIZE_EVALUATE == 0:
+            dict_evaluate = evaluate_model(model)
+            print("iteration", i, "train_loss", dict_evaluate["train"], "test_loss", dict_evaluate["test"])
+
+        xtr, ytr = batch
+        model.zero_grad()
+
+        log_probs, loss = model(xtr, ytr)
+
+        loss.backward()
+        optimizer.step()
+    print(loss)
+
+tokens_single_char = torch.zeros((1,1), dtype = torch.long)
+sample = model.generate(input = tokens_single_char, size_context=SIZE_CONTEXT, max_new_tokens = 500) # 2d tensor
+sample_output = _tokenizer.character_decode(sample[0].tolist(), dict_to_token)
+print(sample_output)
--- nanoGPT/models.py
+++ nanoGPT/models.py
@@ -84,4 +84,58 @@ class Head(torch.nn.Module):
 
         return out
 
+class BigramLanguageModelAttention(torch.nn.Module):
+    """
+    Treat logits = C[X]
+    Then P = softmax(C[X])
 
+    Weak since tokens don't talk to each other
+    """
+    def __init__(self, size_context, num_embeddings, size_embedding, size_head):
+        super().__init__()
+        self.embedding_tokens = torch.nn.Embedding(num_embeddings=num_embeddings, embedding_dim=size_embedding)
+        self.embedding_token_position = torch.nn.Embedding(num_embeddings=size_context, embedding_dim=size_embedding)
+        self.self_attention_head = Head(size_context, size_embedding, size_head)
+        self.projection_decoder = torch.nn.Linear(size_head, num_embeddings)
+
+    def forward(self, input, targets):
+        """
+        Inputs/Targets are of shape (size_batch, size_context)
+        Reshape things accordingly for torch.nn.functional.cross_entropy(., C, ...)
+
+        cross_entropy = NLL(softmax( logits ))
+        NOTE: Remember, pytorch calls matmul()
+        """
+        B, T = input.shape
+        tokens = self.embedding_tokens(input) # (input.shape(), Channels) = (B, T, size_embedding)
+        positions = self.embedding_token_position(torch.arange(T)) # (input.shape(), Channels) = (T, size_embedding)
+        input_embeddings = tokens + positions # broadcasted to (B, T, size_embedding)
+        attention = self.self_attention_head(input_embeddings) # (B, T, d) 
+        # size_head = size_embeddings
+        logits = self.projection_decoder(attention) # (B, T, d)  * (d, num_embeddings)^T -> (B, T, num_embeddings)
+
+        loss = None
+
+        if targets is not None:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+
+            targets = targets.view(B*T)
+            loss = torch.nn.functional.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, input, size_context, max_new_tokens):
+        """
+        input is passed to (T, size_embedding) and (T, size_embedding)
+        """
+        tensor_out = input.clone()
+        for i in range(max_new_tokens):
+            tensor_input = tensor_out[:, -size_context:]
+            logits, loss = self(tensor_input, None) # log counts
+            logits = logits[:,-1,:] # last token in each sequence reduce to (batch_size, num_embeddings)
+            P = torch.nn.functional.softmax(logits, dim=-1) # exp(log_counts) / row_sum = P
+            tensor_out_next = torch.multinomial(P, num_samples=1, replacement=True)
+            tensor_out = torch.cat([tensor_out, tensor_out_next], dim = 1)
+        
+        return tensor_out
